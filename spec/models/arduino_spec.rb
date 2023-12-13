ab4c3210c2e02fc8a61756ec71b0453906f09dd5
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-13 16:45:22 -0300

    4° commit - tdd model: description consegue ser preenchido? RED

--- spec/models/arduino_spec.rb
+++ spec/models/arduino_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Arduino, type: :model do
       expect(@arduino.name).to eq('uno')
     end
 
+    it 'description consegue ser preenchido?' do
+      @arduino.description = ''
+      expect(@arduino.description).to eq('arduino mais usado')
+    end
+
   end
 
 end
